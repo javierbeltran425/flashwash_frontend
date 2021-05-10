ee02caef2031fc2d851f81cf64b4b4cd295eb53a
javierbeltran425 <00019315@uca.edu.sv>
2021-05-09 20:45:16 -0600

    Initial commit

---
+++ flashwashfrontend/src/App.js
@@ -0,0 +1,14 @@
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { QueryClientProvider, QueryClient } from 'react-query'
+
+function App() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
