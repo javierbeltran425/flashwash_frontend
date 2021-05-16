624f0951765249d411a54304627e1ad1b7a5eff4
Andrea-09 <00366519@uca.edu.sv>
2021-05-15 23:14:59 -0600

    Vista de escritorio

--- flashwashfrontend/src/Components/Footer.js
+++ flashwashfrontend/src/Components/Footer.js
@@ -1,14 +1,39 @@
 import FactCard from './FactCard'
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+
+
 const Footer = () => {
     return(
-        <>
-            <div className="absolute ml-20 flex items-center h-1/4 w-72 bottom-0 overflow-x-auto">
+        <> 
+     
+            <div className="hidden lg:flex lg:absolute lg:w-1/4 bottom-0 left-0 mb-5  flex-row items-center">
+            <p className="transform -rotate-90 lg:text-white lg:p-3 xs:hidden">Contacto</p>
+            <div className="flex flex-wrap flew-col p-1">
+            <p className=" lg:text-white left-0 p-1 "><Icon icon={faWhatsapp} className="text-green-600 text-2xl"></Icon>Whatsapp</p>
+            <p className=" lg:text-white left-0 p-2 bottom-0">0000-1111</p>
+            </div>
+            <div className="flex flex-wrap flew-col p-1">
+            <p className=" lg:text-white left-0 p-3"><Icon icon={faInstagram} className="text-pink-500 text-2xl "></Icon>Instagram</p>
+            <p className=" lg:text-white left-0 p-2 bottom-0 ">@Flash_Wash</p>
+            </div>
+            <div className="flex flex-wrap flew-col p-1">
+            <p className=" lg:text-white left-0 p-1 "><Icon icon={faFacebook} className="text-indigo-600 text-2xl "></Icon>Facebook</p>
+            <p className=" lg:text-white left-0 p-2 bottom-0">Flash Wash</p>
+            </div>
+            <p className="hidden lg:flex transform -rotate-90 lg:text-white lg:p-1 mb-1 z-10"> Promociones</p>
+            </div>
+            
+            <div className="absolute ml-20 lg:bottom-0 right-0 lg:w-2/3 lg:h-30 flex items-center h-1/4 w-72 bottom-0 overflow-x-auto" >
                 <FactCard />
                 <FactCard />
                 <FactCard />
                 <FactCard />
                 <FactCard />
             </div>
+     
         </>
     )
 }
--- flashwashfrontend/src/Components/SideBar.js
+++ flashwashfrontend/src/Components/SideBar.js
@@ -1,12 +1,12 @@
 const SideBar = () => {
     return (
       <>
-        <nav className="absolute left-0 w-1/5 h-screen  flex justify-center px-2 py-3 bg-blue-300 mb-1 z-10">
+        <nav className="absolute lg:w-24 lg:bg-opacity-0 lg:h-4/6 lg:mb-5 left-0 w-1/5 h-screen  flex justify-center px-2 py-3 bg-blue-300 mb-1 z-10">
             <ul className="flex flex-wrap items-center my-10 mx-1">
-              <li className="transform -rotate-90"><a href="#Pablo">Acerca de nosotros</a></li>
-              <li className="transform -rotate-90"><a href="#Pablo">Servicios</a></li>
-              <li className="transform -rotate-90"><a href="#Pablo">Productos</a></li>
-              <li className="transform -rotate-90"><a href="#Pablo">Contacto</a></li>
+              <li className="transform -rotate-90 lg:text-white"><a href="#Pablo">Acerca de nosotros</a></li>
+              <li className="transform -rotate-90 lg:text-white"><a href="#Pablo">Servicios</a></li>
+              <li className="transform -rotate-90 lg:text-white"><a href="#Pablo">Productos</a></li>
+              <li className="transform -rotate-90 lg:text-white lg:hidden"><a href="#Pablo">Contacto</a></li>
             </ul>
         </nav>
       </>
