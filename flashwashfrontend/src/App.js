cdf3f728a74b523442f5147797a5f223d6b1df96
Andrea-09 <00366519@uca.edu.sv>
2021-06-08 12:15:09 -0600

    Services view escritorio

--- flashwashfrontend/src/App.js
+++ flashwashfrontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Home from './Pages/Home'
 import About from './Pages/About'
+import Services from './Pages/Services'
 
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { QueryClientProvider, QueryClient } from 'react-query'
@@ -14,6 +15,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home}/>
           <Route exact path="/about" component={About}/>
+          <Route exact path="/services" component={Services}/>
         </Switch>
       </Router>
     </>
