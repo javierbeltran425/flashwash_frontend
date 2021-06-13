6c3fd72e331ea19db058d8171c0d48e5591d6d25
Andrea-09 <00366519@uca.edu.sv>
2021-06-12 21:48:19 -0600

    Carousel product, services

--- flashwashfrontend/src/Pages/Home.js
+++ flashwashfrontend/src/Pages/Home.js
@@ -5,9 +5,29 @@ import SideBar from '../Components/SideBar'
 
 import banner from '../Resources/CarWash4.jpeg'
 import FactCard from '../Components/FactCard'
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 import { useHistory } from 'react-router-dom'
 import { useState } from 'react'
+
+const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      slidesToSlide: 3, // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 2, // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+  };
 
 
 const Home = () => {
@@ -19,8 +39,13 @@ const Home = () => {
             <div className="flex w-full h-screen overflow-hidden justify-center items-center bg-gray-800">
                 <img src={banner} className="hidden lg:flex lg:w-full lg:h-screen opacity-20"></img>
                 <SideBar/>
-                <div className="flex justify-center w-auto h-1/2 mb-20 md:hidden lg:hidden">
+                <div className="lg:hidden flex ml-20 justify-center items-center w-full h-full mb-20 md:hidden lg:hidden">
+                    <Carousel className="w-9/12" responsive={responsive}>
                     <GenCard />
+                    <GenCard />
+                    <GenCard />
+                    <GenCard />
+                    </Carousel>
                 </div>
                 <div className="hidden absolute lg:flex lg:right-0 lg:top-12 lg:opacity-80">
                     <FactCard />
